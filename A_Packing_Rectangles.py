cb5e60223c4f32cc22266094bbdec8f4a7edd044
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-03-15 21:12:29 +0300

    added the first  question for the binary search edu step 2 on codeforce

---
+++ A_Packing_Rectangles.py
@@ -0,0 +1,18 @@
+w , h , n = map(int , input().split())
+
+def check(x):
+    if (x//w) * (x//h) >= n:
+        return 1 
+    else:
+        return 0
+    
+l , r = 0 , max(w,h)*n
+
+while l < r:
+    md = (l + r) // 2 
+
+    if check(md):
+        r = md
+    else:
+        l = md+1
+print(r)
