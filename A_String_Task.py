47eed6092a6da3b3ad19d815bd77aeb25a87ff03
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-03-20 09:50:31 +0300

    added my template

--- A_String_Task.py
+++ A_String_Task.py
@@ -1,3 +1,12 @@
 import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right as br, bisect_left as bl; from math import inf; import math; 
 ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip()
  
+arr = cip()
+ans = []
+v = {"A", "O", "Y", "E", "U", "I"}
+for n in arr:
+    if n.upper() not in v :
+        ans.append(".")
+        ans.append(n.lower())
+        
+print("".join(ans))
