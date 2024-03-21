d1b72c09e23a17cc365df67d91c1b10307b03e29
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-03-21 21:10:51 +0300

    added more question

--- A_New_Year_Candles.py
+++ A_New_Year_Candles.py
@@ -1,14 +1,12 @@
 import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; import math; 
 ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip()
 
+a , b = tip()
+x= a
 
-n , a , b , c = tip()
-ans = 0 
-mxa = a // 2
-mic = max(0, (n - a // 2 - b) // 2)
-for i in range(mic, min(n // 2, c) + 1):
-    nab = n - i * 2
-    ans += max(0, min(nab, b) - max(0, nab - mxa) + 1)
-print(ans)
-                    
+while(a>=b):
+    x= x+(a//b)
+    a=(a//b) + (a%b)
+
+print(x)
 
