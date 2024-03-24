313ae43bb562708e9b83690dfdb651ff6b37e5d3
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-03-24 20:24:33 +0300

    added more question

---
+++ A_Cheap_Travel.py
@@ -0,0 +1,10 @@
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; import math; 
+ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip()
+
+n , m , a , b = tip()
+
+t1 = (n//m * b)+ min((n %m)*a , b)
+
+t2 = n * a 
+
+print(min(t1 , t2))
