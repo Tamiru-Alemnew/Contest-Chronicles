43f69fa1a4c0d335e6f623e58e82f59c450a726d
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-03-26 22:07:39 +0300

    added more questions

--- E_Machine_Testing.py
+++ E_Machine_Testing.py
@@ -1,44 +1,26 @@
-import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; import math; 
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math; 
 ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip()
 
 for _ in range(ip()):
     n = ip()
     health = lip()
     power = lip()
-    if n <= 1 :
-        print(0)
-        continue
 
-    stack = []
-    prev_time = math.ceil(health[1] / power[0])
-    stack.append((prev_time, power[1]))
+    stack = [(inf, power[0])]
+    ans = 0
+    for i in range(1, n):
+        duration = 0
+        while health[i] - (stack[-1][0] - duration)*stack[-1][-1] > 0:
+            t, p = stack.pop()
 
-
-    for idx in range(2, n):
-        curr_time = math.ceil(health[idx] / power[idx - 1])
-
-        if curr_time < prev_time:
-            stack.append((curr_time, power[idx]))
-
-        else:
-            prev_time, prev_power = stack.pop()
-            curr_health = health[idx] - prev_time * prev_power
-
-            while stack and curr_health > 0:
-                cur_time , cur_power = stack.pop()
-                curr_health -=(cur_time - prev_time ) * cur_power
-                prev_time = cur_time
-            
-            if not stack:
-                if curr_health > 0 :
-                    stack.append((cur_time + math.ceil(cur_time / power[0]) , power[idx]))
-            else:
-                stack.append((cur_time , power[idx]))
-
+            health[i] -= (t-duration)*p
+            duration += (t-duration)
+       
+        duration += ceil(health[i]/stack[-1][-1])
+        stack.append((duration, power[i]))
+        ans = max(ans, duration)
         print(stack)
-
-
-    print(max(stack))
+    print(ans)
 
 
 
