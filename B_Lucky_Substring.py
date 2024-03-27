daf02627d818696e085b457a5ec490fb6d903eb6
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-03-27 06:38:51 +0300

    added more questions

---
+++ B_Lucky_Substring.py
@@ -0,0 +1,20 @@
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math; 
+ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip()
+
+
+n = cip()
+
+count = Counter(n)
+
+if count["4"] or count['7']:
+    if count['7'] > count['4']:
+        print(7)
+    else:
+        print(4)
+
+else:
+    print(-1)
+
+
+
+
