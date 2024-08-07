e879166223922449b43e3158a7fcfb274cd270ac
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-07 11:23:32 +0300

    add(at coder): dp questions upto 3

--- B_Frog_2.py
+++ B_Frog_2.py
@@ -1,14 +1,12 @@
 import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math;from heapq import *;ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0;
-n = ip()
 
-cost = tip()
-
-dp = [0]*n
+n , k = tip()
+cost = lip()
+dp = [inf]*n
 dp[0] = 0
-for i in range(1 , n ):
-    if i >= 2:
-        dp[i] = min(abs(cost[i] - cost[i-1] ) + dp[i-1], dp[i-2] +  abs(cost[i] -cost[i-2]))
-    else:
-        dp[i] = abs(cost[i] - cost[i-1] ) + dp[i-1]
+
+for i in range(1 , n):
+    for j in range(1 , min(k , i) +1):
+        dp[i] = min(dp[i] , abs(cost[i] - cost[i - j]) + dp[i-j])
 
 print(dp[-1])
--- C_Vacation.py
+++ C_Vacation.py
@@ -1,14 +1,24 @@
 import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math;from heapq import *;ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0;
 n = ip()
+a = []
+b = []
+c = []
+for i in range(n):
+    aa , bb , cc = tip()
+    a.append(aa)
+    b.append(bb)
+    c.append(cc)
 
-cost = tip()
+dpa = [0]*n
+dpb = [0]*n
+dpc = [0]*n
 
-dp = [0]*n
-dp[0] = 0
-for i in range(1 , n ):
-    if i >= 2:
-        dp[i] = min(abs(cost[i] - cost[i-1] ) + dp[i-1], dp[i-2] +  abs(cost[i] -cost[i-2]))
-    else:
-        dp[i] = abs(cost[i] - cost[i-1] ) + dp[i-1]
+dpa[0] = a[0]
+dpb[0] = b[0]
+dpc[0] = c[0]
+for i in range(1 , n):
+    dpa[i] = a[i] + max(dpb[i-1] , dpc[i-1])
+    dpb[i] = b[i] + max(dpa[i-1] , dpc[i-1])
+    dpc[i] = c[i] + max(dpa[i-1] , dpb[i-1])
 
-print(dp[-1])
+print(max(dpa[-1] , dpb[-1] , dpc[-1]))
